55268219014d0cc88669f6ca061aec4854e3c45d
Mike Peel <email@mikepeel.net>
2017-09-04 16:26:11 -0300

    new

--- fitspectrum/run_smoothmaps.py
+++ fitspectrum/run_smoothmaps.py
@@ -8,15 +8,15 @@ output_nside = 512
 directory = '/scratch/nas_cbass/scratch/mpeel/smoothmaps/'
 
 # WMAP 9-year maps
-smoothmap(directory+'wmap_band_imap_r9_9yr_K_v5.fits',directory+'512_60.00smoothed_wmap9K.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),pol=False,nside_out=output_nside,nobsmap=1)
+smoothmap(directory+'wmap_band_imap_r9_9yr_K_v5.fits',directory+'512_60.00smoothed_wmap9K.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),nside_out=output_nside,nobsmap=1)
 
-smoothmap(directory+'wmap_band_imap_r9_9yr_Ka_v5.fits',directory+'512_60.00smoothed_wmap9Ka.fits', np.sqrt(output_resolution**2-(0.66*60.0)**2),pol=False,nside_out=output_nside,nobsmap=1)
+smoothmap(directory+'wmap_band_imap_r9_9yr_Ka_v5.fits',directory+'512_60.00smoothed_wmap9Ka.fits', np.sqrt(output_resolution**2-(0.66*60.0)**2),nside_out=output_nside,nobsmap=1)
 
-smoothmap(directory+'wmap_band_imap_r9_9yr_Q_v5.fits',directory+'512_60.00smoothed_wmap9Q.fits', np.sqrt(output_resolution**2-(0.51*60.0)**2),pol=False,nside_out=output_nside,nobsmap=1)
+smoothmap(directory+'wmap_band_imap_r9_9yr_Q_v5.fits',directory+'512_60.00smoothed_wmap9Q.fits', np.sqrt(output_resolution**2-(0.51*60.0)**2),nside_out=output_nside,nobsmap=1)
 
-smoothmap(directory+'wmap_band_imap_r9_9yr_V_v5.fits',directory+'512_60.00smoothed_wmap9V.fits', np.sqrt(output_resolution**2-(0.35*60.0)**2),pol=False,nside_out=output_nside,nobsmap=1)
+smoothmap(directory+'wmap_band_imap_r9_9yr_V_v5.fits',directory+'512_60.00smoothed_wmap9V.fits', np.sqrt(output_resolution**2-(0.35*60.0)**2),nside_out=output_nside,nobsmap=1)
 
-smoothmap(directory+'wmap_band_imap_r9_9yr_W_v5.fits',directory+'512_60.00smoothed_wmap9W.fits', np.sqrt(output_resolution**2-(0.22*60.0)**2),pol=False,nside_out=output_nside,nobsmap=1)
+smoothmap(directory+'wmap_band_imap_r9_9yr_W_v5.fits',directory+'512_60.00smoothed_wmap9W.fits', np.sqrt(output_resolution**2-(0.22*60.0)**2),nside_out=output_nside,nobsmap=1)
 
 smoothmap(directory+'LFI_SkyMap_030_1024_R2.01_full.fits',directory+'512_60.00smoothed_LFI_SkyMap_30_256_PR2.01_full.fits',np.sqrt(output_resolution**2-34.2**2),nside_out=output_nside,units_out='mK_RJ')
 
