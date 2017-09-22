d241ac8e5a3edb13ab53cef9fadf281692d894b9
Michael Peel <mpeel@vpn72.its.manchester.ac.uk>
2017-09-22 15:19:11 -0300

    wmap

--- fitspectrum/run_smoothmaps.py
+++ fitspectrum/run_smoothmaps.py
@@ -9,18 +9,36 @@ output_nside = 256
 directory = '/mirror/data/mpeel/smoothmaps/'
 
 
-beamtf = np.loadtxt('wmap_ampl_bl_K1_9yr_v5p1.txt',usecols=(1))
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9_K_testdecon.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK',nosmooth=[0])
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9_K_testbeam.fits',output_resolution,nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf**2)
-smoothmap(directory+'wmap_band_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9_K_testbeam1.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK')
+beamtf_K = np.loadtxt(directory+'wmap_ampl_bl_K1_9yr_v5p1.txt',usecols=(1,))
+beamtf_Ka = np.loadtxt(directory+'wmap_ampl_bl_Ka1_9yr_v5p1.txt',usecols=(1,))
+beamtf_Q = np.loadtxt(directory+'wmap_ampl_bl_Q1_9yr_v5p1.txt',usecols=(1,))
+beamtf_V = np.loadtxt(directory+'wmap_ampl_bl_V1_9yr_v5p1.txt',usecols=(1,))
+beamtf_W = np.loadtxt(directory+'wmap_ampl_bl_W1_9yr_v5p1.txt',usecols=(1,))
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9dec_22.8_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK',nosmooth=[0])
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Ka_v5.fits',directory+'256_60.00smoothed_wmap9dec_33.0_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.66*60.0)**2),nside_out=output_nside,sigma_0=1.466,sigma_0_unit='mK',nosmooth=[0])
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Q_v5.fits',directory+'256_60.00smoothed_wmap9dec_40.7_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.51*60.0)**2),nside_out=output_nside,sigma_0=2.188,sigma_0_unit='mK',nosmooth=[0])
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_V_v5.fits',directory+'256_60.00smoothed_wmap9dec_60.7_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.35*60.0)**2),nside_out=output_nside,sigma_0=3.131,sigma_0_unit='mK',nosmooth=[0])
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_W_v5.fits',directory+'256_60.00smoothed_wmap9dec_93.5_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.22*60.0)**2),nside_out=output_nside,sigma_0=6.544,sigma_0_unit='mK',nosmooth=[0])
+
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9decbeam_22.8_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf_K**2)
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Ka_v5.fits',directory+'256_60.00smoothed_wmap9decbeam_33.0_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=1.466,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf_Ka**2)
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Q_v5.fits',directory+'256_60.00smoothed_wmap9decbeam_40.7_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=2.188,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf_Q**2)
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_V_v5.fits',directory+'256_60.00smoothed_wmap9decbeam_60.7_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=3.131,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf_V**2)
+smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_W_v5.fits',directory+'256_60.00smoothed_wmap9decbeam_93.5_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=6.544,sigma_0_unit='mK',nosmooth=[0],windowfunction=beamtf_W**2)
+
+smoothmap(directory+'wmap_band_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9_22.8_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.88*60.0)**2),nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK')
+smoothmap(directory+'wmap_band_imap_r9_9yr_Ka_v5.fits',directory+'256_60.00smoothed_wmap9_33.0_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.66*60.0)**2),nside_out=output_nside,sigma_0=1.466,sigma_0_unit='mK')
+smoothmap(directory+'wmap_band_imap_r9_9yr_Q_v5.fits',directory+'256_60.00smoothed_wmap9_40.7_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.51*60.0)**2),nside_out=output_nside,sigma_0=2.188,sigma_0_unit='mK')
+smoothmap(directory+'wmap_band_imap_r9_9yr_V_v5.fits',directory+'256_60.00smoothed_wmap9_60.7_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.35*60.0)**2),nside_out=output_nside,sigma_0=3.131,sigma_0_unit='mK')
+smoothmap(directory+'wmap_band_imap_r9_9yr_W_v5.fits',directory+'256_60.00smoothed_wmap9_93.5_512_2013_mKCMBunits.fits', np.sqrt(output_resolution**2-(0.22*60.0)**2),nside_out=output_nside,sigma_0=6.544,sigma_0_unit='mK')
+
+smoothmap(directory+'wmap_band_imap_r9_9yr_K_v5.fits',directory+'256_60.00smoothed_wmap9beam_22.8_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=1.429,sigma_0_unit='mK',windowfunction=beamtf_K**2)
+smoothmap(directory+'wmap_band_imap_r9_9yr_Ka_v5.fits',directory+'256_60.00smoothed_wmap9beam_33.0_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=1.466,sigma_0_unit='mK',windowfunction=beamtf_Ka**2)
+smoothmap(directory+'wmap_band_imap_r9_9yr_Q_v5.fits',directory+'256_60.00smoothed_wmap9beam_40.7_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=2.188,sigma_0_unit='mK',windowfunction=beamtf_Q**2)
+smoothmap(directory+'wmap_band_imap_r9_9yr_V_v5.fits',directory+'256_60.00smoothed_wmap9beam_60.7_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=3.131,sigma_0_unit='mK',windowfunction=beamtf_V**2)
+smoothmap(directory+'wmap_band_imap_r9_9yr_W_v5.fits',directory+'256_60.00smoothed_wmap9beam_93.5_512_2013_mKCMBunits.fits', output_resolution,nside_out=output_nside,sigma_0=6.544,sigma_0_unit='mK',windowfunction=beamtf_W**2)
 
 exit()
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Ka_v5.fits',directory+'256_60.00smoothed_wmap9Ka.fits', np.sqrt(output_resolution**2-(0.66*60.0)**2),nside_out=output_nside,sigma_0=1.466,sigma_0_unit='mK',nosmooth=[0])
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_Q_v5.fits',directory+'256_60.00smoothed_wmap9Q.fits', np.sqrt(output_resolution**2-(0.51*60.0)**2),nside_out=output_nside,sigma_0=2.188,sigma_0_unit='mK',nosmooth=[0])
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_V_v5.fits',directory+'256_60.00smoothed_wmap9V.fits', np.sqrt(output_resolution**2-(0.35*60.0)**2),nside_out=output_nside,sigma_0=3.131,sigma_0_unit='mK',nosmooth=[0])
-smoothmap(directory+'wmap_band_deconv_imap_r9_9yr_W_v5.fits',directory+'256_60.00smoothed_wmap9W.fits', np.sqrt(output_resolution**2-(0.22*60.0)**2),nside_out=output_nside,sigma_0=6.544,sigma_0_unit='mK',nosmooth=[0])
-
-
 
 # smoothmap(directory+'LFI_SkyMap_030_1024_R2.01_full.fits',directory+'256_60.00smoothed_LFI_30_256_PR2.01_full_test1.fits',np.sqrt(output_resolution**2-34.2**2),nside_out=output_nside)#,units_out='mK_RJ')
 smoothmap(directory+'LFI_SkyMap_030_1024_R2.01_full.fits',directory+'256_60.00smoothed_LFI_30_256_PR2.01_full_test2a.fits',np.sqrt(output_resolution**2-34.2**2),nside_out=output_nside,outputmaps=[0,4])
