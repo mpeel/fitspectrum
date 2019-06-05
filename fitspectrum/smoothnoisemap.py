d087dff26e64585eac655ee1453a097a518ee569
Mike Peel <email@mikepeel.net>
2019-06-05 17:31:06 +0100

    +

--- fitspectrum/smoothnoisemap.py
+++ fitspectrum/smoothnoisemap.py
@@ -7,11 +7,13 @@
 # Mike Peel    17 Sep 2017    ud_grade to use constant nsides
 # Mike Peel    04 Oct 2017    Optimising and debugging. Added multiple nside support.
 # Mike Peel    06 Oct 2017    Return to older variance calculation, add rescale param and change output name format
+# Mike Peel    05 Jun 2019    v0.4 Add taper, cope with cut sky maps
 
 import numpy as np
 import healpy as hp
-from smoothmap import smoothmap, conv_nobs_variance_map
+from astrocode.fitspectrum.smoothmap import smoothmap, conv_nobs_variance_map
 import astropy.io.fits as fits
+import os
 
 def noiserealisation(inputmap, numpixels):
     newmap = np.zeros(numpixels)
@@ -19,8 +21,13 @@ def noiserealisation(inputmap, numpixels):
     return newmap
 
 
-def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisations=10, sigma_0 = 0.0, nside=[512], windowfunction = [], rescale=1.0):
-    ver = "0.3"
+def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisations=10, sigma_0 = 0.0, nside=[512], windowfunction = [], rescale=1.0,usehealpixfits=False,taper=False,lmin_taper=350,lmax_taper=600):
+    ver = "0.4"
+
+    if (os.path.isfile(indir+"/"+runname+"_actualvariance.fits")):
+        print("You already have a file with the output name " + indir+"/"+runname+"_actualvariance.fits" + "! Not going to overwrite it. Move it, or set a new output filename, and try again!")
+        # exit()
+        return
 
     # Read in the input map
     inputfits = fits.open(indir+"/"+inputmap)
@@ -28,13 +35,20 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
     col_names = cols.names
     nmaps = len(cols)
     maps = []
-    for i in range(0,nmaps):
-        maps.append(inputfits[1].data.field(i))
+    if usehealpixfits:
+        maps = hp.read_map(indir+inputmap,field=None)
+    else:
+        for i in range(0,nmaps):
+            maps.append(inputfits[1].data.field(i))
     nside_in = hp.get_nside(maps)
 
     # Check to see whether we have nested data, and switch to ring if that is the case.
     if (inputfits[1].header['ORDERING'] == 'NESTED'):
         maps = hp.reorder(maps,n2r=True)
+
+    maps[mapnumber][maps[mapnumber]<-1e10] = hp.UNSEEN
+    maps[mapnumber][~np.isfinite(maps[mapnumber])] = hp.UNSEEN
+    map_before = maps[mapnumber].copy()
 
     if sigma_0 != 0.0:
         # If we have a value for sigma_0, then we have an Nobs map and need to convert it.
@@ -48,7 +62,7 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
     cols = []
     cols.append(fits.Column(name='II_cov', format='E', array=np.square(noisemap)))
     cols = fits.ColDefs(cols)
-    bin_hdu = fits.new_table(cols)
+    bin_hdu = fits.BinTableHDU.from_columns(cols)
     bin_hdu.header['ORDERING']='RING'
     bin_hdu.header['POLCONV']='COSMO'
     bin_hdu.header['PIXTYPE']='HEALPIX'
@@ -60,7 +74,7 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
     cols = []
     cols.append(fits.Column(name='II_cov', format='E', array=conv_nobs_variance_map(np.square(noisemap), sigma_0)))
     cols = fits.ColDefs(cols)
-    bin_hdu = fits.new_table(cols)
+    bin_hdu = fits.BinTableHDU.from_columns(cols)
     bin_hdu.header['ORDERING']='RING'
     bin_hdu.header['POLCONV']='COSMO'
     bin_hdu.header['PIXTYPE']='HEALPIX'
@@ -80,27 +94,40 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
         conv_windowfunction[windowfunction!=0] /= windowfunction[windowfunction!=0]
         conv_windowfunction[windowfunction==0] = 0.0
     conv_windowfunction /= conv_windowfunction[0]
+    # If needed, apply a taper
+    if taper:
+        conv_windowfunction[lmin_taper:lmax_taper] = conv_windowfunction[lmin_taper:lmax_taper] * np.cos((np.pi/2.0)*((np.arange(lmin_taper,lmax_taper)-lmin_taper)/(lmax_taper-lmin_taper)))
+        conv_windowfunction[lmax_taper:] = 0.0
 
     # Now generate the noise realisations
     numpixels = len(noisemap)
     returnmap = np.zeros(numpixels)
+    # print(np.min(noisemap))
+    noisemap[map_before == hp.UNSEEN] = 0.0
+    hp.write_map(indir+"/"+runname+"_noisemap.fits",noisemap,overwrite=True)
+    alms = hp.map2alm(noisemap)#,lmax=4*nside_in)
+    alms = hp.almxfl(alms, conv_windowfunction)
+    newnoisemap = hp.alm2map(alms, nside_in)#,lmax=4*nside_in)
+    hp.write_map(indir+"/"+runname+"_newnoisemap.fits",newnoisemap,overwrite=True)
+    exit()
     for i in range(0,numrealisations):
         if i%10==0:
-            print i
+            print(i)
         # Generate the noise realisation
         newmap = noiserealisation(noisemap, numpixels)
         # smooth it
-        alms = hp.map2alm(newmap,lmax=4*nside_in)
+        alms = hp.map2alm(newmap)#,lmax=4*nside_in)
         alms = hp.almxfl(alms, conv_windowfunction)
-        newmap = hp.alm2map(alms, nside_in,lmax=4*nside_in)
+        newmap = hp.alm2map(alms, nside_in)#,lmax=4*nside_in)
         returnmap = returnmap + np.square(newmap)
     returnmap = returnmap/(numrealisations-1)
+    returnmap[map_before == hp.UNSEEN] = hp.UNSEEN
     
     # All done - now just need to write it to disk.
     cols = []
     cols.append(fits.Column(name='II_cov', format='E', array=returnmap))
     cols = fits.ColDefs(cols)
-    bin_hdu = fits.new_table(cols)
+    bin_hdu = fits.BinTableHDU.from_columns(cols)
     bin_hdu.header['ORDERING']='RING'
     bin_hdu.header['POLCONV']='COSMO'
     bin_hdu.header['PIXTYPE']='HEALPIX'
@@ -113,7 +140,7 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
     cols = []
     cols.append(fits.Column(name='II_nobs', format='E', array=nobs_map))
     cols = fits.ColDefs(cols)
-    bin_hdu = fits.new_table(cols)
+    bin_hdu = fits.BinTableHDU.from_columns(cols)
     bin_hdu.header['ORDERING']='RING'
     bin_hdu.header['POLCONV']='COSMO'
     bin_hdu.header['PIXTYPE']='HEALPIX'
@@ -131,7 +158,7 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
         cols = []
         cols.append(fits.Column(name='II_cov', format='E', array=returnmap_ud))
         cols = fits.ColDefs(cols)
-        bin_hdu = fits.new_table(cols)
+        bin_hdu = fits.BinTableHDU.from_columns(cols)
         bin_hdu.header['ORDERING']='RING'
         bin_hdu.header['POLCONV']='COSMO'
         bin_hdu.header['PIXTYPE']='HEALPIX'
@@ -144,7 +171,7 @@ def smoothnoisemap(indir, runname, inputmap, mapnumber=2, fwhm=0.0, numrealisati
         cols = []
         cols.append(fits.Column(name='II_nobs', format='E', array=nobs_map))
         cols = fits.ColDefs(cols)
-        bin_hdu = fits.new_table(cols)
+        bin_hdu = fits.BinTableHDU.from_columns(cols)
         bin_hdu.header['ORDERING']='RING'
         bin_hdu.header['POLCONV']='COSMO'
         bin_hdu.header['PIXTYPE']='HEALPIX'
